2056fbef8264eded84a2fc59aa2593b1fc9fffa5
o3o-wc <o3o-wc@naver.com>
2020-10-08 14:29:36 +0900

    edit
    edit

---
+++ Portfolio/js/script.js
@@ -0,0 +1,10 @@
+(function(){
+    const leftbtn = document.querySelector(".left")
+    const rightbtn = document.querySelector(".right")
+    leftbtn.addEventListener("click",function(){
+        console.log(leftbtn)
+    })
+    rightbtn.addEventListener("click",function(){
+        console.log(rightbtn)
+    })
+})()
