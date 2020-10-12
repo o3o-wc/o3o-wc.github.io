528f38f0fd4b0908457c2f0d7d1fa2693fea35e3
o3o-wc <o3o-wc@naver.com>
2020-10-12 17:47:20 +0900

    edit
    edit

--- Portfolio/js/mordal.js
+++ Portfolio/js/mordal.js
@@ -1,11 +1,31 @@
 (function(){
     const closebtn = document.querySelector(".close")
     const modal =document.querySelector(".modal")
+    const modalimg = modal.getElementsByTagName("img")
     const img=document.querySelectorAll(".rtg")
+    const btn=document.querySelector(".btnlink")
+    let link=  btn.getAttribute("href")
 
     for(let i=0;i<img.length;i++){
     img[i].addEventListener("click",function(e){
-        console.log(e.target)
+        let num = e.target.getAttribute("alt");
+        console.log(btn)
+        if(num == 1){
+            btn.setAttribute.href="https://o3o-wc.github.io/web/index.html";
+        }
+        if(num == 2){
+            btn.href="111";
+            btn.classList.add("hide")
+        }
+        if(num == 3){
+            btn.href="https://o3o-wc.github.io/grid/grid.html";
+        }
+        if(num == 4){
+            btn.href="https://o3o-wc.github.io/Momentum/index.html";
+        }
+        if(num == 5){
+            btn.href="https://o3o-wc.github.io/interactive/index.html";
+        }
         modal.classList.add("on");
         document.body.classList.add("hidescroll")
     })
@@ -13,6 +33,7 @@
 
     closebtn.addEventListener("click",function(){
         modal.classList.remove("on");
+        btn.classList.remove("hide")
         document.body.classList.remove("hidescroll");
     })
 })()
